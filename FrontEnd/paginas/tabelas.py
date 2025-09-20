c8404496d9b234d5f2c42e00087c5b0820cdd031
mendesanabeatrz <mendesanabeatrz@gmail.com>
2025-09-20 18:37:43 -0300

    [Gabriel] FrontEnd

--- FrontEnd/paginas/tabelas.py
+++ FrontEnd/paginas/tabelas.py
@@ -5,38 +5,35 @@ import pandas as pd
 
 BASE_URL = "http://127.0.0.1:8000/api/v1"
 
-st.title("APS")
+def show():
+    endpoints = {
+        "Pacientes": "pacientes",
+        "Hospitais": "hospitais",
+        "Médicos": "medicos",
+        "Especialidades": "especialidades",
+        "Hospitais-Especialidades": "hospitais_especialidades",
+        "Médicos-Hospitais": "medicos_hospitais",
+        "Pacientes-Hospitais": "pacientes_hospitais",
+        "CID-10": "cid10",
+        "Estados": "estados",
+        "Municípios": "municipios",
+    }
 
-st.subheader("Agência Premiersoft de saúde")
+    abas = st.tabs(list(endpoints.keys()))
 
-endpoints = {
-    "Pacientes": "pacientes",
-    "Hospitais": "hospitais",
-    "Médicos": "medicos",
-    "Especialidades": "especialidades",
-    "Hospitais-Especialidades": "hospitais_especialidades",
-    "Médicos-Hospitais": "medicos_hospitais",
-    "Pacientes-Hospitais": "pacientes_hospitais",
-    "CID-10": "cid10",
-    "Estados": "estados",
-    "Municípios": "municipios",
-}
+    for nome, aba in zip(endpoints.keys(), abas):
+        with aba:
+            st.write(f" Dados de **{nome}**")
+        
+            try:
+                response = requests.get(f"{BASE_URL}/{endpoints[nome]}")
+                response.raise_for_status()
+                data = response.json()
+                if isinstance(data, list) and len(data) > 0:
+                    df = pd.DataFrame(data)
+                    st.dataframe(df, use_container_width=True)
+                else:
+                    st.info("Nenhum dado encontrado.")
 
-abas = st.tabs(list(endpoints.keys()))
-
-for nome, aba in zip(endpoints.keys(), abas):
-    with aba:
-        st.write(f" Dados de **{nome}**")
-    
-        try:
-            response = requests.get(f"{BASE_URL}/{endpoints[nome]}")
-            response.raise_for_status()
-            data = response.json()
-            if isinstance(data, list) and len(data) > 0:
-                df = pd.DataFrame(data)
-                st.dataframe(df, use_container_width=True)
-            else:
-                st.info("Nenhum dado encontrado.")
-
-        except requests.exceptions.RequestException as e:
-            st.error(f"Erro ao conectar no backend: {e}")
+            except requests.exceptions.RequestException as e:
+                st.error(f"Erro ao conectar no backend: {e}")
