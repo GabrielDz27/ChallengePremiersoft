40cbbf64bc1e4a149984fb46626f0c3d76f1614c
Bernardo Mesko <bemesko@sou.cloud>
2025-09-20 18:15:00 -0300

    Cria docker compose básico pro backend

--- BackEnd/database.py
+++ BackEnd/database.py
@@ -1,11 +1,13 @@
 # backend/database.py
 
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql://root:123456@localhost:3306/data_saude"
+DATABASE_CONNECTION_STRING = os.getenv("DATABASE_CONNECTION_STRING")
 
-engine = create_engine("mysql+pymysql://root:123456@localhost:3306/data_saude", echo=True)
+engine = create_engine(DATABASE_CONNECTION_STRING, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
