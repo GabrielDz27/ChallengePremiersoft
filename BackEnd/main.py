57101618f1a7369650cf174de06aa2124fa4dc93
Gustavo R Bachmann <53021714+CodeBachmann@users.noreply.github.com>
2025-09-21 01:21:14 -0300

    Update main.py

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,6 +1,6 @@
 from sys import prefix
 from fastapi import FastAPI
-from routers import medicos, pacientes
+from routers import medicos, pacientes, hospitais
 
 app = FastAPI(
 )
@@ -9,7 +9,7 @@ prefix="/api/v1"
 
 app.include_router(pacientes.router, prefix=prefix)
 app.include_router(medicos.router, prefix=prefix)
-
+app.include_router(hospitais.router, prefix=prefix)
 # Inclua outros routers conforme necessário
 
 if __name__ == "__main__":
